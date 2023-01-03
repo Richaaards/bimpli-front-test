dd93a1a0b96c01960a5f5bc399d3fee40e8d10c2
Richard Phonthibsvads <richards@MacBook-Pro-de-Richard.local>
2023-01-03 12:29:53 +0100

    display restaurant info page with mock data

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app" class="w-full">
-      <restaurants-list />    
+      <!-- <restaurants-list />
+      <restaurant-info /> -->
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import RestaurantsList from './pages/RestaurantsList'
+// import RestaurantsList from './pages/RestaurantsList'
+// import RestaurantInfo from './pages/RestaurantInfo'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    RestaurantsList,
+    // RestaurantsList,
+    // RestaurantInfo,
   }
 }
 </script>
 
 <style>
-#app {
+
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/pages/RestaurantInfo.vue
+++ src/pages/RestaurantInfo.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>        
-        <h2>Restaurant name</h2>
-        <h3>Restaurant location</h3>
+    <div class="flex flex-wrap p-8">        
+        <section
+            class="overflow-hidden bg-no-repeat bg-cover w-full"
+            style="
+                background-position: 50%;
+                background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/146.webp');
+                height: 500px;
+            "
+        >
+        </section>
+
+        <section class="my-8 w-full text-left">
+            <h2 class="text-3xl font-bold">{{ this.$route.params.restaurant.name }}</h2>
+            <h3 class="text-2xl uppercase font-normal">{{ this.$route.params.restaurant.location }}</h3>
+        </section>
     </div>
 </template>
 
